fd869c93e7ec13700087f69aedf1bbfd7d7a4e55
gbauer9 <bauerga@onid.oregonstate.edu>
2017-02-03 04:12:15 -0800

    Added getter and setter functions so variables can be accessed outside of object

--- src/main/java/edu/oregonstate/cs361/battleship/Position.java
+++ src/main/java/edu/oregonstate/cs361/battleship/Position.java
@@ -5,13 +5,22 @@ import com.google.gson.annotations.SerializedName;
 
 public class Position {
 	@SerializedName("Across")
-	public int across;
+	private int across;
 	@SerializedName("Down")
-	public int down;
+	private int down;
 
-	public Position() { this(across: 0, down: 0); }
+	public Position(){
+		this(0, 0);
+	}
+
 	public Position(int across, int down) {
 		this.across = across;
 		this.down = down;
 	}
+
+	//Getter and setter functions for easy access to variables
+	public int getAcross() { return this.across; }
+	public int getDown() { return this.down; }
+	public void setAcross( int a ) { this.across = a; }
+	public void setDown( int d ) { this.down = d; }
 }
